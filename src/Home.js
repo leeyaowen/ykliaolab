9cf4447361c258f2355beb41cfe8fdb8967cea50
leeyaowen <jcjh30101@gmail.com>
2021-01-11 17:42:17 +0800

    made changes

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import './Home.css';
+import {Helmet} from "react-helmet";
+
+
+function Home() {
+    return(
+        <div className='App'>
+            <Helmet>
+                <title>林木生理遺傳研究室-clone</title>
+            </Helmet>
+            <h1>123</h1>
+        </div>
+    );
+}
+
+export default Home;
