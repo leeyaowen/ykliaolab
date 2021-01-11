686fb0499a40ded62bd400d0bd5610c856ca82aa
leeyaowen <jcjh30101@gmail.com>
2021-01-11 18:07:06 +0800

    made changes

--- src/Routerapp.js
+++ src/Routerapp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Route, Switch} from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Host from "./components/Host";
